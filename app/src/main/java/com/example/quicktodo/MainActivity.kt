d9a5cbd7ae49efad7eb33e8a85d76dfac9a486a8
Ahmed Hamed <ahmedhamed7728@gmail.com>
2023-10-26 23:33:40 +0300

    feat: home screen for todo list

--- app/src/main/java/com/example/quicktodo/MainActivity.kt
+++ app/src/main/java/com/example/quicktodo/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.quicktodo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quicktodo.ui.theme.QuickTodoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +26,9 @@ class MainActivity : ComponentActivity() {
             QuickTodoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
@@ -36,6 +42,32 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         modifier = modifier
     )
 }
+
+
+@Composable
+fun HomeScreen() {
+    fun onPressToCreateTodo() {
+        // Your function logic here
+        Log.d("Button", "Button clicked in HomeScreen!")
+    }
+
+    CreateNewTodoButton(onClick = { onPressToCreateTodo() })
+}
+
+@Composable
+fun CreateNewTodoButton(onClick:() -> Unit ) {
+    Box(modifier = Modifier.fillMaxSize()){
+        Button(onClick = { onClick() }, shape = RoundedCornerShape(50),
+            modifier = Modifier.align(
+            Alignment.BottomEnd)
+                .padding(16.dp) ) {
+            Text("+")
+        }
+    }
+
+}
+
+
 
 @Preview(showBackground = true)
 @Composable
